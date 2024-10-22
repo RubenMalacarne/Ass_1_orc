4d6a9484ef1951738b227435685744d2c55af2fb
Ruben <ruben.malacarne@gmail.com>
2024-10-22 14:14:49 +0200

    standard ex_1

--- talos_conf.py
+++ talos_conf.py
@@ -25,7 +25,7 @@ fMax = 1000.0                               # maximum normal force
 contactNormal = np.array([0., 0., 1.])      # direction of the normal to the contact surface
 
 #weights variables
-w_com = 10.0             # weight of center of mass task
+w_com = 1.0             # weight of center of mass task
 w_am = 1e-3             # weight of angular momentum task
 w_foot = 1e-1           # weight of the foot motion task
 w_contact = -1.0        # weight of foot in contact (negative means infinite weight)
